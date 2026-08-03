f1514e050b09307fe87d1762874ca0e760f63d3e
Kyaw Khine Aung <kyawkhineaung.ucss@gmail.com>
2026-08-03 13:53:47 +0630

    Update project structure and database configuration

--- config/database.php
+++ config/database.php
@@ -1,15 +1,16 @@
-<?php 
+<?php
 define("DB_HOST", "localhost");
 define("DB_NAME", "auth_system");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
 
-function dbConnect() {
+
+function dbConnect()
+{
     $db_connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     if (mysqli_connect_errno() > 0) {
         die("Connection Fail!");
     } else {
         return $db_connect;
     }
-    
 }
